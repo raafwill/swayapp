32bcb736ec47e0f989b5df8941f7ffbce9425c49
willianmanteiga <willianrafel68@gmail.com>
2022-06-22 19:17:17 -0300

    atualizado

--- system/system/settings.py
+++ system/system/settings.py
@@ -130,7 +130,7 @@ USE_THOUSAND_SEPARATOR = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'staticfiles'),
+   os.path.join(BASE_DIR, 'swayapp/static'),
 )
 
 LOGIN_URL = '/admin/login/'
