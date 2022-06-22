67f56e671aa42524ce0f521907b275740a2183d9
willianmanteiga <willianrafel68@gmail.com>
2022-06-22 19:36:30 -0300

    atualizado

--- system/system/settings.py
+++ system/system/settings.py
@@ -127,16 +127,16 @@ USE_THOUSAND_SEPARATOR = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'staticfiles'),
 )
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
+
 LOGIN_URL = '/admin/login/'
 LOGOUT_REDIRECT_URL = '/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
